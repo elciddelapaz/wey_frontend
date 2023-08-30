f960b206471118bba9bb10494f1caadf71c1acd2
elciddelapaz <elcid.delapaz@gmail.com>
2023-08-30 16:37:43 +0800

    feat: friend suggestion

--- src/components/PeopleYouMayKnow.vue
+++ src/components/PeopleYouMayKnow.vue
@@ -1,54 +1,31 @@
+<script setup>
+import axios from 'axios'
+import { onMounted, reactive } from 'vue'
+import { RouterLink } from 'vue-router'
+const users = reactive({ data: [] })
+
+onMounted(() => {
+  axios
+    .get('/api/friends/suggested/')
+    .then((res) => {
+      users.data = res.data
+      // console.log(res.data)
+    })
+    .catch((err) => console.log(err))
+})
+</script>
 <template>
   <div class="p-4 bg-white border border-gray-200 rounded-lg">
     <h3 class="mb-6 text-xl">People you may know</h3>
     <div class="space-y-4">
-      <div class="flex items-center justify-between">
+      <div class="flex items-center justify-between" v-for="user in users.data" :key="user.id">
         <div class="flex items-center space-x-2">
-          <img
-            src="https://i.pravatar.cc/300?img=70"
-            class="w-[40px] rounded-full"
-          />
+          <img :src="user.get_avatar" class="w-[40px] rounded-full" />
           <p class="text-xs">
-            <strong>Code with stein</strong>
+            <strong>{{ user.name }}</strong>
           </p>
         </div>
-        <a
-          href="#"
-          class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg"
-          >Show</a
-        >
-      </div>
-      <div class="flex items-center justify-between">
-        <div class="flex items-center space-x-2">
-          <img
-            src="https://i.pravatar.cc/300?img=70"
-            class="w-[40px] rounded-full"
-          />
-          <p class="text-xs">
-            <strong>Code with stein</strong>
-          </p>
-        </div>
-        <a
-          href="#"
-          class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg"
-          >Show</a
-        >
-      </div>
-      <div class="flex items-center justify-between">
-        <div class="flex items-center space-x-2">
-          <img
-            src="https://i.pravatar.cc/300?img=70"
-            class="w-[40px] rounded-full"
-          />
-          <p class="text-xs">
-            <strong>Code with stein</strong>
-          </p>
-        </div>
-        <a
-          href="#"
-          class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg"
-          >Show</a
-        >
+        <RouterLink :to="{ name: 'profile', params: { id: user.id } }" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">Show</RouterLink>
       </div>
     </div>
   </div>
